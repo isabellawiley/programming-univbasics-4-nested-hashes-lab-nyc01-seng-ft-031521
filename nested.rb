c8709cff6afff70317572d655e2e2c86269c766f
Isabella Wiley <wileyisabella@gmail.com>
2021-03-09 07:38:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -135,7 +135,7 @@ def adding_to_dennis
       }
     }
     
-  programmer_hash[:dennis_ritchie][:languages] += "Assembly"
+  programmer_hash[:dennis_ritchie][:languages].insert(-1,"Assembly")
   programmer_hash
 
 end
